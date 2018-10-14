6a04b4387f87959442f7529c82fe68063bfc16fa
Keivan Hassani Monfared <k1monfared@users.noreply.github.com>
2018-10-14 12:21:29 -0600

    all pages updated

---
+++ css/imageresize.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+function ChangeImageSize(Element, targetWidth){
+        //alert ('Change Size of ' + Element.ID + ' to ' + targetWidth);
+        
+        var newTargetWidth;
+        
+        if (targetWidth > 0.5){ //double it
+            Element.height *= 2;
+            Element.width *= 2;
+            newTargetWidth = targetWidth/2;
+        }else{ //half it
+            Element.height *= .5;
+            Element.width *= .5;
+            newTargetWidth = targetWidth*2;        
+        }
+        
+        Element.onclick = 'ChangeImageSize(this, ' + newTargetWidth +')';  //  Changed it from Element.id to this
+        
+    }
+});
