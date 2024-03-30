a0b2e756eb38f494a625a70bfbb5242fcbbe64da
Keivan <k1monfared@gmail.com>
2024-03-30 15:07:20 -0700

    quick commit

--- css/floating-1.12.js
+++ css/floating-1.12.js
@@ -5,18 +5,15 @@
  * License:
  * GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  */
-var floatingMenu =
-{
+var floatingMenu = {
     hasInner: typeof(window.innerWidth) == 'number',
-    hasElement: typeof(document.documentElement) == 'object'
-        && typeof(document.documentElement.clientWidth) == 'number'};
+    hasElement: typeof(document.documentElement) == 'object' &&
+        typeof(document.documentElement.clientWidth) == 'number'
+};
 
-var floatingArray =
-[
-];
+var floatingArray = [];
 
-floatingMenu.add = function(obj, options)
-{
+floatingMenu.add = function(obj, options) {
     var name;
     var menu;
 
@@ -24,71 +21,62 @@ floatingMenu.add = function(obj, options)
         name = obj;
     else
         menu = obj;
-        
 
-    if (options == undefined)
-    {
-        floatingArray.push( 
-            {
-                id: name,
-                menu: menu,
 
-                targetLeft: 0,
-                targetTop: 0,
+    if (options == undefined) {
+        floatingArray.push({
+            id: name,
+            menu: menu,
 
-                distance: .07,
-                snap: true,
-                updateParentHeight: false
-            });
-    }
-    else
-    {
-        floatingArray.push( 
-            {
-                id: name,
-                menu: menu,
+            targetLeft: 0,
+            targetTop: 0,
 
-                targetLeft: options.targetLeft,
-                targetRight: options.targetRight,
-                targetTop: options.targetTop,
-                targetBottom: options.targetBottom,
+            distance: .07,
+            snap: true,
+            updateParentHeight: false
+        });
+    } else {
+        floatingArray.push({
+            id: name,
+            menu: menu,
 
-                centerX: options.centerX,
-                centerY: options.centerY,
+            targetLeft: options.targetLeft,
+            targetRight: options.targetRight,
+            targetTop: options.targetTop,
+            targetBottom: options.targetBottom,
 
-                prohibitXMovement: options.prohibitXMovement,
-                prohibitYMovement: options.prohibitYMovement,
+            centerX: options.centerX,
+            centerY: options.centerY,
 
-                distance: options.distance != undefined ? options.distance : .07,
-                snap: options.snap,
-                ignoreParentDimensions: options.ignoreParentDimensions,
-                updateParentHeight:
-                    options.updateParentHeight == undefined
-                    ? false
-                    : options.updateParentHeight,
+            prohibitXMovement: options.prohibitXMovement,
+            prohibitYMovement: options.prohibitYMovement,
 
-                scrollContainer: options.scrollContainer,
-                scrollContainerId: options.scrollContainerId,
+            distance: options.distance != undefined ? options.distance : .07,
+            snap: options.snap,
+            ignoreParentDimensions: options.ignoreParentDimensions,
+            updateParentHeight: options.updateParentHeight == undefined ?
+                false :
+                options.updateParentHeight,
 
-                confinementArea: options.confinementArea,
+            scrollContainer: options.scrollContainer,
+            scrollContainerId: options.scrollContainerId,
 
-                confinementAreaId:
-                    options.confinementArea != undefined
-                    && options.confinementArea.substring(0, 1) == '#'
-                    ? options.confinementArea.substring(1)
-                    : undefined,
+            confinementArea: options.confinementArea,
 
-                confinementAreaClassRegexp:
-                    options.confinementArea != undefined
-                    && options.confinementArea.substring(0, 1) == '.'
-                    ? new RegExp("(^|\\s)" + options.confinementArea.substring(1) + "(\\s|$)")
-                    : undefined
-            });
+            confinementAreaId: options.confinementArea != undefined &&
+                options.confinementArea.substring(0, 1) == '#' ?
+                options.confinementArea.substring(1) :
+                undefined,
+
+            confinementAreaClassRegexp: options.confinementArea != undefined &&
+                options.confinementArea.substring(0, 1) == '.' ?
+                new RegExp("(^|\\s)" + options.confinementArea.substring(1) + "(\\s|$)") :
+                undefined
+        });
     }
 };
 
-floatingMenu.findSingle = function(item)
-{
+floatingMenu.findSingle = function(item) {
     if (item.id)
         item.menu = document.getElementById(item.id);
 
@@ -96,133 +84,114 @@ floatingMenu.findSingle = function(item)
         item.scrollContainer = document.getElementById(item.scrollContainerId);
 };
 
-floatingMenu.move = function (item)
-{
-    if (!item.prohibitXMovement)
-    {
+floatingMenu.move = function(item) {
+    if (!item.prohibitXMovement) {
         item.menu.style.left = item.nextX + 'px';
         item.menu.style.right = '';
     }
 
-    if (!item.prohibitYMovement)
-    {
+    if (!item.prohibitYMovement) {
         item.menu.style.top = item.nextY + 'px';
         item.menu.style.bottom = '';
     }
 };
 
-floatingMenu.scrollLeft = function(item)
-{
+floatingMenu.scrollLeft = function(item) {
     // If floating within scrollable container use it's scrollLeft
     if (item.scrollContainer)
         return item.scrollContainer.scrollLeft;
 
     var w = window.top;
 
-    return this.hasInner
-        ? w.pageXOffset  
-        : this.hasElement  
-          ? w.document.documentElement.scrollLeft  
-          : w.document.body.scrollLeft;
+    return this.hasInner ?
+        w.pageXOffset :
+        this.hasElement ?
+        w.document.documentElement.scrollLeft :
+        w.document.body.scrollLeft;
 };
 
-floatingMenu.scrollTop = function(item)
-{
+floatingMenu.scrollTop = function(item) {
     // If floating within scrollable container use it's scrollTop
     if (item.scrollContainer)
         return item.scrollContainer.scrollTop;
 
     var w = window.top;
 
-    return this.hasInner
-        ? w.pageYOffset
-        : this.hasElement
-          ? w.document.documentElement.scrollTop
-          : w.document.body.scrollTop;
+    return this.hasInner ?
+        w.pageYOffset :
+        this.hasElement ?
+        w.document.documentElement.scrollTop :
+        w.document.body.scrollTop;
 };
 
-floatingMenu.windowWidth = function()
-{
-    return this.hasElement
-        ? document.documentElement.clientWidth
-        : document.body.clientWidth;
+floatingMenu.windowWidth = function() {
+    return this.hasElement ?
+        document.documentElement.clientWidth :
+        document.body.clientWidth;
 };
 
-floatingMenu.windowHeight = function()
-{
-    if (floatingMenu.hasElement && floatingMenu.hasInner)
-    {
+floatingMenu.windowHeight = function() {
+    if (floatingMenu.hasElement && floatingMenu.hasInner) {
         // Handle Opera 8 problems
-        return document.documentElement.clientHeight > window.innerHeight
-            ? window.innerHeight
-            : document.documentElement.clientHeight
-    }
-    else
-    {
-        return floatingMenu.hasElement
-            ? document.documentElement.clientHeight
-            : document.body.clientHeight;
+        return document.documentElement.clientHeight > window.innerHeight ?
+            window.innerHeight :
+            document.documentElement.clientHeight
+    } else {
+        return floatingMenu.hasElement ?
+            document.documentElement.clientHeight :
+            document.body.clientHeight;
     }
 };
 
-floatingMenu.documentHeight = function()
-{
-    var innerHeight = this.hasInner
-        ? window.innerHeight
-        : 0;
+floatingMenu.documentHeight = function() {
+    var innerHeight = this.hasInner ?
+        window.innerHeight :
+        0;
 
     var body = document.body,
         html = document.documentElement;
 
     return Math.max(
         body.scrollHeight,
-        body.offsetHeight, 
+        body.offsetHeight,
         html.clientHeight,
         html.scrollHeight,
         html.offsetHeight,
         innerHeight);
 };
 
-floatingMenu.documentWidth = function()
-{
-    var innerWidth = this.hasInner
-        ? window.innerWidth
-        : 0;
+floatingMenu.documentWidth = function() {
+    var innerWidth = this.hasInner ?
+        window.innerWidth :
+        0;
 
     var body = document.body,
         html = document.documentElement;
 
     return Math.max(
         body.scrollWidth,
-        body.offsetWidth, 
+        body.offsetWidth,
         html.clientWidth,
         html.scrollWidth,
         html.offsetWidth,
         innerWidth);
 };
 
-floatingMenu.calculateCornerX = function(item)
-{
+floatingMenu.calculateCornerX = function(item) {
     var offsetWidth = item.menu.offsetWidth;
 
     var result = this.scrollLeft(item) - item.parentLeft;
 
-    if (item.centerX)
-    {
-        result += (this.windowWidth() - offsetWidth)/2;
-    }
-    else if (item.targetLeft == undefined)
-    {
+    if (item.centerX) {
+        result += (this.windowWidth() - offsetWidth) / 2;
+    } else if (item.targetLeft == undefined) {
         result += this.windowWidth() - item.targetRight - offsetWidth;
-    }
-    else
-    {
+    } else {
         result += item.targetLeft;
     }
-        
-    if (document.body != item.menu.parentNode
-        && result + offsetWidth >= item.confinedWidthReserve)
-    {
+
+    if (document.body != item.menu.parentNode &&
+        result + offsetWidth >= item.confinedWidthReserve) {
         result = item.confinedWidthReserve - offsetWidth;
     }
 
@@ -232,54 +201,44 @@ floatingMenu.calculateCornerX = function(item)
     return result;
 };
 
-floatingMenu.calculateCornerY = function(item)
-{
+floatingMenu.calculateCornerY = function(item) {
     var offsetHeight = item.menu.offsetHeight;
 
     var result = this.scrollTop(item) - item.parentTop;
 
-    if (item.centerY)
-    {
-        result += (this.windowHeight() - offsetHeight)/2;
-    }
-    else if (item.targetTop === undefined)
-    {
+    if (item.centerY) {
+        result += (this.windowHeight() - offsetHeight) / 2;
+    } else if (item.targetTop === undefined) {
         result += this.windowHeight() - item.targetBottom - offsetHeight;
-    }
-    else
-    {
+    } else {
         result += item.targetTop;
     }
 
-    if (document.body != item.menu.parentNode
-        && result + offsetHeight >= item.confinedHeightReserve)
-    {
+    if (document.body != item.menu.parentNode &&
+        result + offsetHeight >= item.confinedHeightReserve) {
         result = item.confinedHeightReserve - offsetHeight;
     }
 
     if (result < 0)
         result = 0;
-        
+
     return result;
 };
 
-floatingMenu.isConfinementArea = function(item, area)
-{
-    return item.confinementAreaId != undefined
-        && area.id == item.confinementAreaId
-        || item.confinementAreaClassRegexp != undefined
-        && area.className
-        && item.confinementAreaClassRegexp.test(area.className);
+floatingMenu.isConfinementArea = function(item, area) {
+    return item.confinementAreaId != undefined &&
+        area.id == item.confinementAreaId ||
+        item.confinementAreaClassRegexp != undefined &&
+        area.className &&
+        item.confinementAreaClassRegexp.test(area.className);
 };
 
-floatingMenu.computeParent = function(item)
-{
-    if (item.ignoreParentDimensions)
-    {
+floatingMenu.computeParent = function(item) {
+    if (item.ignoreParentDimensions) {
         item.confinedHeightReserve = this.documentHeight();
         item.confinedWidthReserver = this.documentWidth();
-        item.parentLeft = 0;  
-        item.parentTop = 0;  
+        item.parentLeft = 0;
+        item.parentTop = 0;
         return;
     }
 
@@ -297,37 +256,30 @@ floatingMenu.computeParent = function(item)
     var obj = parentNode;
     var objOffsets = this.offsets(obj, item);
 
-    if (item.confinementArea == undefined)
-    {
-        while (obj.clientHeight + objOffsets.top
-                   < item.menu.scrollHeight + parentOffsets.top
-               || item.menu.parentNode == obj
-               && item.updateParentHeight
-               && obj.clientHeight + objOffsets.top
-                   == item.menu.scrollHeight + parentOffsets.top)
-        {
+    if (item.confinementArea == undefined) {
+        while (obj.clientHeight + objOffsets.top <
+            item.menu.scrollHeight + parentOffsets.top ||
+            item.menu.parentNode == obj &&
+            item.updateParentHeight &&
+            obj.clientHeight + objOffsets.top ==
+            item.menu.scrollHeight + parentOffsets.top) {
             obj = obj.parentNode;
             objOffsets = this.offsets(obj, item);
         }
-    }
-    else
-    {
-        while (obj.parentNode != undefined
-               && !this.isConfinementArea(item, obj))
-        {
+    } else {
+        while (obj.parentNode != undefined &&
+            !this.isConfinementArea(item, obj)) {
             obj = obj.parentNode;
             objOffsets = this.offsets(obj, item);
         }
     }
 
-    item.confinedHeightReserve = obj.clientHeight
-        - (parentOffsets.top - objOffsets.top);
+    item.confinedHeightReserve = obj.clientHeight -
+        (parentOffsets.top - objOffsets.top);
 };
 
-floatingMenu.offsets = function(obj, item)
-{
-    var result =
-    {
+floatingMenu.offsets = function(obj, item) {
+    var result = {
         left: 0,
         top: 0
     };
@@ -335,42 +287,37 @@ floatingMenu.offsets = function(obj, item)
     if (obj === item.scrollContainer)
         return;
 
-    while (obj.offsetParent && obj.offsetParent != item.scrollContainer)
-    {  
-        result.left += obj.offsetLeft;  
-        result.top += obj.offsetTop;  
+    while (obj.offsetParent && obj.offsetParent != item.scrollContainer) {
+        result.left += obj.offsetLeft;
+        result.top += obj.offsetTop;
         obj = obj.offsetParent;
-    }  
+    }
 
     if (window == window.top)
         return result;
 
     // we're IFRAMEd
     var iframes = window.top.document.body.getElementsByTagName("IFRAME");
-    for (var i = 0; i < iframes.length; i++)
-    {
+    for (var i = 0; i < iframes.length; i++) {
         if (iframes[i].contentWindow != window)
-           continue;
+            continue;
 
         obj = iframes[i];
-        while (obj.offsetParent)  
-        {  
-            result.left += obj.offsetLeft;  
-            result.top += obj.offsetTop;  
+        while (obj.offsetParent) {
+            result.left += obj.offsetLeft;
+            result.top += obj.offsetTop;
             obj = obj.offsetParent;
-        }  
+        }
     }
 
     return result;
 };
 
-floatingMenu.doFloatSingle = function(item)
-{
+floatingMenu.doFloatSingle = function(item) {
     this.findSingle(item);
 
-    if (item.updateParentHeight)
-    {
-        item.menu.parentNode.style.minHeight = 
+    if (item.updateParentHeight) {
+        item.menu.parentNode.style.minHeight =
             item.menu.scrollHeight + 'px';
     }
 
@@ -381,54 +328,43 @@ floatingMenu.doFloatSingle = function(item)
     var cornerX = this.calculateCornerX(item);
 
     var stepX = (cornerX - item.nextX) * item.distance;
-    if (Math.abs(stepX) < .5 && item.snap
-        || Math.abs(cornerX - item.nextX) <= 1)
-    {
+    if (Math.abs(stepX) < .5 && item.snap ||
+        Math.abs(cornerX - item.nextX) <= 1) {
         stepX = cornerX - item.nextX;
     }
 
     var cornerY = this.calculateCornerY(item);
 
     var stepY = (cornerY - item.nextY) * item.distance;
-    if (Math.abs(stepY) < .5 && item.snap
-        || Math.abs(cornerY - item.nextY) <= 1)
-    {
+    if (Math.abs(stepY) < .5 && item.snap ||
+        Math.abs(cornerY - item.nextY) <= 1) {
         stepY = cornerY - item.nextY;
     }
 
     if (Math.abs(stepX) > 0 ||
-        Math.abs(stepY) > 0)
-    {
+        Math.abs(stepY) > 0) {
         item.nextX += stepX;
         item.nextY += stepY;
         this.move(item);
     }
 };
 
-floatingMenu.fixTargets = function()
-{
-};
+floatingMenu.fixTargets = function() {};
 
-floatingMenu.fixTarget = function(item)
-{
-};
+floatingMenu.fixTarget = function(item) {};
 
-floatingMenu.doFloat = function()
-{
+floatingMenu.doFloat = function() {
     this.fixTargets();
-    for (var i=0; i < floatingArray.length; i++)
-    {
+    for (var i = 0; i < floatingArray.length; i++) {
         this.fixTarget(floatingArray[i]);
         this.doFloatSingle(floatingArray[i]);
     }
     setTimeout('floatingMenu.doFloat()', 20);
 };
 
-floatingMenu.insertEvent = function(element, event, handler)
-{
+floatingMenu.insertEvent = function(element, event, handler) {
     // W3C
-    if (element.addEventListener != undefined)
-    {
+    if (element.addEventListener != undefined) {
         element.addEventListener(event, handler, false);
         return;
     }
@@ -436,30 +372,26 @@ floatingMenu.insertEvent = function(element, event, handler)
     var listener = 'on' + event;
 
     // MS
-    if (element.attachEvent != undefined)
-    {
+    if (element.attachEvent != undefined) {
         element.attachEvent(listener, handler);
         return;
     }
 
     // Fallback
     var oldHandler = element[listener];
-    element[listener] = function (e)
-        {
-            e = (e) ? e : window.event;
-            var result = handler(e);
-            return (oldHandler != undefined) 
-                && (oldHandler(e) == true)
-                && (result == true);
-        };
+    element[listener] = function(e) {
+        e = (e) ? e : window.event;
+        var result = handler(e);
+        return (oldHandler != undefined) &&
+            (oldHandler(e) == true) &&
+            (result == true);
+    };
 };
 
-floatingMenu.init = function()
-{
+floatingMenu.init = function() {
     floatingMenu.fixTargets();
 
-    for (var i=0; i < floatingArray.length; i++)
-    {
+    for (var i = 0; i < floatingArray.length; i++) {
         floatingMenu.initSingleMenu(floatingArray[i]);
     }
 
@@ -468,8 +400,7 @@ floatingMenu.init = function()
 
 // Some browsers init scrollbars only after
 // full document load.
-floatingMenu.initSingleMenu = function(item)
-{
+floatingMenu.initSingleMenu = function(item) {
     this.findSingle(item);
     this.computeParent(item);
     this.fixTarget(item);
@@ -482,16 +413,12 @@ floatingMenu.insertEvent(window, 'load', floatingMenu.init);
 
 
 // Register ourselves as jQuery plugin if jQuery is present
-if (typeof(jQuery) !== 'undefined')
-{
-    (function ($)
-    {
-        $.fn.addFloating = function(options)
-        {
-            return this.each(function()
-            {
+if (typeof(jQuery) !== 'undefined') {
+    (function($) {
+        $.fn.addFloating = function(options) {
+            return this.each(function() {
                 floatingMenu.add(this, options);
             });
         };
-    }) (jQuery);
+    })(jQuery);
 }
--- css/imageresize.js
+++ css/imageresize.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-function ChangeImageSize(Element, targetWidth){
+$(document).ready(function() {
+    function ChangeImageSize(Element, targetWidth) {
         //alert ('Change Size of ' + Element.ID + ' to ' + targetWidth);
-        
+
         var newTargetWidth;
-        
-        if (targetWidth > 0.5){ //double it
+
+        if (targetWidth > 0.5) { //double it
             Element.height *= 2;
             Element.width *= 2;
-            newTargetWidth = targetWidth/2;
-        }else{ //half it
+            newTargetWidth = targetWidth / 2;
+        } else { //half it
             Element.height *= .5;
             Element.width *= .5;
-            newTargetWidth = targetWidth*2;        
+            newTargetWidth = targetWidth * 2;
         }
-        
-        Element.onclick = 'ChangeImageSize(this, ' + newTargetWidth +')';  //  Changed it from Element.id to this
-        
+
+        Element.onclick = 'ChangeImageSize(this, ' + newTargetWidth + ')'; //  Changed it from Element.id to this
+
     }
 });
